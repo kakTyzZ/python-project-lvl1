4cc3ad12dc8606ab940f9083dc0ea7ae8fecd2a0
Yakov <kaktyzz-imba@mail.ru>
2019-12-29 00:42:05 +0300

    gg

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,6 @@
 import prompt
 
+
 def run():
-	name = prompt.string('May i have your name? ')
-	print(f"Hello, {name}!")
-	
-
-
+    name = prompt.string('May i have your name? ')
+    print(f"Hello, {name}!")

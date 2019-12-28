f57b4938e56cbad54b73003f92584f3a0f6a189d
Yakov <kaktyzz-imba@mail.ru>
2019-12-29 00:42:26 +0300

    gg

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,17 +1,13 @@
 
 #!/home/yakov/project/brain_games/scripts python3
-from brain_games.cli  import run
-
-
+from brain_games.cli import run
 
 
 def main():
     print("Welcome to the brain Games!")
 
 
-
-
-
 if __name__ == '__main__':
     main()
+
 run()

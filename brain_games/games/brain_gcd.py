8b289f9d19f5fb35a43339be0a4889d3ae01a10b
kaktyzz <kaktyzz-imba@mail.ru>
2020-04-25 14:08:42 +0300

    new1

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,53 +1,19 @@
 #!/usr/bin/env python
 import random
-from .brain_even import correct
 from brain_games.cli import name
 import prompt
-from .brain_calc import invalid
+from brain_games.scripts.brain import main,gcd_user_question,gcd_answer,nod
+
+print("Find the greatest common divisor of given numbers.")
 
 
-def nod(a, b):
-    # Check the lowest number
-    if a < b:
-        x = a
-    else:
-        x = b
-    # Add numbers to a list
-    l1 = []
-    for i in range(x):
-        l1.append(i + 1)
-    l2 = l1[::-1]
-    for i in l2:
-        if a % i == 0 and b % i == 0:
-            return i
-            break
 
 
-def main():
-    def wrong():
-        return print("You are wrong!")
-    print(f"Hello,{name}!")
-    print("Find the greatest common divisor of given numbers.")
-    TIMES_PLAYED = 0
-    ANSWER = 0
-    while TIMES_PLAYED < 3:
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
-        RIGHT_ANSWER = nod(num1, num2)
-        print(f"question: {num1},{num2}")
-        USER_ANSWER = prompt.integer("Your answer: ")
-        if RIGHT_ANSWER == USER_ANSWER:
-            correct()
-            TIMES_PLAYED += 1
-            ANSWER += 1
-        elif USER_ANSWER != RIGHT_ANSWER:
-            wrong()
-            TIMES_PLAYED += 1
-        else:
-            invalid()
-        if ANSWER == 3:
-            print(f"Congratulations, {name}!")
+
+main(gcd_answer,gcd_user_question)
 
 
 if __name__ == "__main__":
     main()
+
+
--- brain_games/scripts/brain.py
+++ brain_games/scripts/brain.py
@@ -1,8 +1,73 @@
-
+import random
+import prompt
 
 def welcome():
     return "Welcome to the brain Games!"
 
 
+def nod(a, b):
+    # Check the lowest number
+    if a < b:
+        x = a
+    else:
+        x = b
+    # Add numbers to a list
+    l1 = []
+    for i in range(x):
+        l1.append(i + 1)
+    l2 = l1[::-1]
+    for i in l2:
+        if a % i == 0 and b % i == 0:
+            return i
+            break
+        
+        
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 100)  
+      
+def gcd_answer():
+    RIGHT_ANSWER = nod(num1, num2)
+    return RIGHT_ANSWER
+
+def gcd_user_question():
+    print(f"question: {num1},{num2}")
+
+
+
+
+def main(*args):
+    def wrong():
+        return print("You are wrong!")
+    
+    def invalid():
+        return print("Invalid")
+
+    def correct():
+        return print("Correct!")
+    times_played = 0
+    while times_played < 3:
+        for i in args:
+            right_answer = args[0]
+            q = args[1]
+        user_right_answer = 0
+        right_answer = right_answer()
+        print(q())
+        USER_ANSWER = prompt.integer("Your answer: ")
+        if right_answer > USER_ANSWER:
+            correct()
+            user_right_answer += 1
+            times_played += 1
+        elif right_answer < USER_ANSWER:
+            wrong()
+            times_played += 1    
+        else:
+            invalid()
+                
+        if user_right_answer == 3:
+            print(f"Congratulations, {name}!")
+            
 if __name__ == "__main__":
-    welcome()
+    main()
+            
+
+

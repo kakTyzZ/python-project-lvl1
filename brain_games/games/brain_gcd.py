494b09d5d38de1c624554f2646a9d7f9f5e7272d
kaktyzz <kaktyzz-imba@mail.ru>
2020-04-28 23:07:22 +0300

    april28

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -1,15 +1,56 @@
 #!/usr/bin/env python
 import random
-from brain_games.cli import name
 import prompt
-from brain_games.scripts.brain import main,nod
+from brain_games.scripts.brain import main
 
 print("Find the greatest common divisor of given numbers.")
 
+def nod(a, b):
+    # Check the lowest number
+    if a < b:
+        x = a
+    else:
+        x = b
+    # Add numbers to a list
+    l1 = []
+    for i in range(x):
+        l1.append(i + 1)
+    l2 = l1[::-1]
+    for i in l2:
+        if a % i == 0 and b % i == 0:
+            return i
+            break
+
+
+def random_num1():
+    num1 = random.randint(1, 100)
+    return num1
+num1 = random_num1()
+def random_num2():
+    num2 = random.randint(1, 100)
+    return num2
+num2 = random_num2()
+    
+
+
+def gcd_answer(num1,num2):
+    right_answer = nod(num1, num2)
+    return right_answer  
+right_answer1 = gcd_answer(num1,num2)
 
 
 
-main()
+
+def question_num(num1,num2):
+    num = str(num1) + "," + str(num2)
+    return print(f"Question:{num}")
+question = question_num(num1,num2)
+
+
+print("test")
+
+
+main(right_answer1)
 
 
 if __name__ == "__main__":
--- brain_games/scripts/brain.py
+++ brain_games/scripts/brain.py
@@ -1,42 +1,16 @@
 import random
 import prompt
+from brain_games.cli import name
 
 def welcome():
     return "Welcome to the brain Games!"
 
+welcome()
+print(f"Hello,{name}!")
 
-def nod(a, b):
-    # Check the lowest number
-    if a < b:
-        x = a
-    else:
-        x = b
-    # Add numbers to a list
-    l1 = []
-    for i in range(x):
-        l1.append(i + 1)
-    l2 = l1[::-1]
-    for i in l2:
-        if a % i == 0 and b % i == 0:
-            return i
-            break
-        
-        
-
-      
-
-
-
-
-
-
-
-
-def main():
+def main(right_answer):
     
-    def gcd_answer():
-        right_answer = nod(num1, num2)
-        return right_answer  
+    
     def wrong():
         return print("You are wrong!")
     
@@ -48,16 +22,13 @@ def main():
     times_played = 0
     user_right_answer = 0
     while times_played < 3:
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
-        num = str(num1) + "," + str(num2)
-        print(f"question: {num}")
-        USER_ANSWER = prompt.integer("Your answer: ")
-        if right_answer > USER_ANSWER:
+        right_answer = right_answer1
+        user_answer = prompt.integer("Your answer: ")
+        if right_answer == user_answer:
             correct()
             user_right_answer += 1
             times_played += 1
-        elif right_answer < USER_ANSWER:
+        elif right_answer != user_answer:
             wrong()
             times_played += 1    
         else:

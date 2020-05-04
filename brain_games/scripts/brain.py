7633fce9462b2d096dcd42f861caa3556858befd
kaktyzz <kaktyzz-imba@mail.ru>
2020-05-04 18:05:45 +0300

    lastlast3

--- brain_games/scripts/brain.py
+++ brain_games/scripts/brain.py
@@ -1,20 +1,17 @@
-import random
 import prompt
 
 
 def welcome():
-    return "Welcome to the brain Games!"
+    return print("Welcome to the brain Games!")
 
 
+def run(name=None, right_answer=None, question=None):
 
-def main(name=None,right_answer=None):
-    
-    
     print(f"Hello,{name}!")
-    
+
     def wrong():
         return print("You are wrong!")
-    
+
     def invalid():
         return print("Invalid")
 
@@ -22,26 +19,28 @@ def main(name=None,right_answer=None):
         return print("Correct!")
     times_played = 0
     user_right_answer = 0
+    num = 0
     while times_played < 3:
-        
-        user_answer = prompt.integer("Your answer: ")
-        if right_answer == user_answer:
+        print("Question:{}".format(question[num]))
+        if type(right_answer[0]) == int:
+            user_answer = prompt.integer("Your answer: ")
+        else:
+            user_answer = prompt.string("Your answer: ")
+        if right_answer[num] == user_answer:
             correct()
             user_right_answer += 1
             times_played += 1
-        elif right_answer != user_answer:
+            num += 1
+        elif right_answer[num] != user_answer:
             wrong()
-            times_played += 1    
+            times_played += 1
+            num += 1
         else:
             invalid()
-                
+
         if user_right_answer == 3:
             print(f"Congratulations, {name}!")
-            
-         
-            
+
+
 if __name__ == "__main__":
-    main()
-            
-
-
+    run(name=None, right_answer=None, question=None)
